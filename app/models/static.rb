3e369f313a48cf5ab1fe27edab81bbf1031fccc0
Fila Luka <fila.luka@gmail.com>
2010-01-05 14:03:50 +0200

    added sorting to static pages

--- app/models/static.rb
+++ app/models/static.rb
@@ -1,14 +1,9 @@
 class Static < ActiveRecord::Base
   translates :name, :text
-  acts_as_tree :order => "position ASC"
+  acts_as_tree :order => :position
+  acts_as_list :scope => :parent
   validates_presence_of :name, :message => "має бути присутня"
+  validates_uniqueness_of :parent_id, :message => "Тільки одна сторінка може бути головною, виберіть поле 'Предок'", :if => Proc.new {|w| w.parent_id.nil?}
 
-
-
-  def before_save
-    #checking if we got any root there
-
-    errors.add_to_base("Тільки одна сторінка може бути головною, виберіть поле 'Предок'") if self.parent_id.nil?
-  end
 
 end
